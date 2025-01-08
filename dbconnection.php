b33fe962e8e9433aa1aa155b335462313f8904d7
BONFACEVULU <bonywhisky9@gmail.com>
2025-01-08 18:44:40 +0300

    connecting db

---
+++ dbconnection.php
@@ -0,0 +1,19 @@
+<?php
+class dbconnection{
+private $servername = "localhost:3307";
+private $username = "root";
+private $password = "1234";
+private $dbname = "api_moodle";
+
+public function connect(){
+try {
+    $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+}
+}
+?>
