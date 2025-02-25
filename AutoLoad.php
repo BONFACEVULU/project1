ddf1b9c3eb376a94f6237c08523fa5388836aa7c
BONFACEVULU <bonywhisky9@gmail.com>
2025-02-25 15:25:20 +0300

    autoload

---
+++ AutoLoad.php
@@ -0,0 +1,17 @@
+<?php
+// Include Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Method Auto Load
+function classAutoLoad($classname){
+    $directories = ["classes", "contents", "forms", "processes", "global", "menus"];
+    foreach($directories as $dir){
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) && is_readable($filename)){
+            require_once($filename);
+            return;
+        }
+    }
+}
+spl_autoload_register('classAutoLoad');
+?>
